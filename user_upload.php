064b501b657d3f0cb7e5646d78aee30eec17328b
57lax <mateusz,antczak14@gmail.com>
2018-11-09 00:53:38 +0100

    Function documentation added

--- user_upload.php
+++ user_upload.php
@@ -32,7 +32,14 @@
     );
     $options = getopt($shortopts, $longopts);
 
-    function connect_to_DB($servername, $user, $password){
+    /**
+     * Establish connection with DB.
+     *
+     * @param string $servername host name.
+     * @param string $user user name for the database.
+     * @param string $password password to user account.
+     */
+    function connectToDb($servername, $user, $password){
         $db_connection = null;
         $dsn_options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
@@ -45,6 +52,13 @@
         return $db_connection;
     }
 
+    /**
+     * Check if a table exists in the current database.
+     *
+     * @param PDO $pdo PDO instance connected to a database.
+     * @param string $table Table to search for.
+     * @return bool TRUE if table exists, FALSE if no table found.
+     */
     function tableExists($pdo, $table) {
         try {
             $result = $pdo->query("SELECT 1 FROM $table LIMIT 1");
@@ -55,7 +69,14 @@
         return $result !== FALSE;
     }
 
-    function drop_table(&$pdo){
+    /**
+     * Creates the new table in DB.
+     *
+     * @param PDO $pdo PDO instance connected to a database.
+     * @param string $table Table to search for.
+     * @return bool TRUE if table exists, FALSE if no table found.
+     */
+    function dropTable(&$pdo){
         try {
             $statement = $pdo->prepare('DROP TABLE '.TABLE_NAME.';');
             $statement->execute();
@@ -65,7 +86,14 @@
         echo "Table dropped \n";
     }
 
-    function create_table(&$pdo){
+    /**
+     * Creates the new table in DB.
+     *
+     * @param PDO $pdo PDO instance connected to a database.
+     * @param string $table Table to search for.
+     * @return bool TRUE if table exists, FALSE if no table found.
+     */
+    function createTable(&$pdo){
         $create_table = true; 
         if(tableExists($pdo, TABLE_NAME))
         {
@@ -73,7 +101,7 @@
             $ans = fgets(STDIN);
             if(preg_replace('/\s+/', '',$ans) == "y") 
             {
-                drop_table($pdo);
+                dropTable($pdo);
             }
             else 
             {
@@ -96,7 +124,13 @@
         }
     }
 
-    function insert_row($data, &$pdo){
+    /**
+     * Inesrts one row to the users table.
+     *
+     * @param array $data Row (user) data.
+     * @param PDO $pdo PDO instance connected to a database.
+     */
+    function insertRow($data, &$pdo){
         $statement = $pdo->prepare('INSERT INTO '.TABLE_NAME.'(email, name, surname) VALUES (:email, :name, :surname)');
     
         $statement->execute([
@@ -106,7 +140,14 @@
         ]);
     }
 
-    function parse_file($file_name, $dry_run, &$pdo){
+    /**
+     * Parses the csv file provided in the script parameter.
+     *
+     * @param array $data Row (user) data.
+     * @param bool $dry_run If TRUE we only print the data to the screen witout saving it in DB.
+     * @param PDO $pdo PDO instance connected to a database.
+     */
+    function parseFile($file_name, $dry_run, &$pdo){
         $row_no = 0;
         $emails = array();
         $inserted = 0;
@@ -116,7 +157,7 @@
             {
                 if($row_no)
                 {
-                    $data = correct_row($data);
+                    $data = correctRow($data);
                     $validation_data = validate_row($data, $emails);
 
                     if($dry_run) {
@@ -132,13 +173,13 @@
                         // Insert if no errors, do not insert otherwise
                         if(!count($critical_errors) > 0) 
                         {
-                            insert_row($data, $pdo);
+                            insertRow($data, $pdo);
                             $inserted++;
                         } 
                         else 
                         {
                             echo "Row $row_no NOT INSERTED Reason: ";
-                            echo  display_validation($critical_errors);
+                            echo  displayValidation($critical_errors);
                             echo "\n";
                         }
                     }
@@ -148,35 +189,64 @@
         }
         fclose($handle);
         echo "Total number of ".$inserted. " rows inserted. ";
-
     }
 
+    /**
+     * Prints out the data of the user.
+     *
+     * @param array $data Row (user) data.
+     * @param array $validation Validation data about the fields of the user.
+     * @param integer $row_no Validation data about the fields of the user.
+     */
     function display($data, $validation, $row_no){
-        display_row($data, $row_no);
-        display_validation($validation);
+        displayRow($data, $row_no);
+        displayValidation($validation);
         echo " \n";
     }
 
-    function display_row($data, $row_no) {
+    /**
+     * Displays one row of parsed file.
+     *
+     * @param array $data Row (user) data.
+     * @param integer $row_no Row number of data to display.
+     */
+    function displayRow($data, $row_no) {
         $format = '%4u. %-10s %-15s %-25s';
         echo sprintf($format, $row_no, $data[NAME_POSITION], $data[SURNAME_POSITION], $data[EMAIL_POSITION]);
-        //echo $row_no.". ".$data[NAME_POSITION]." " .$data[SURNAME_POSITION]."   \t".$data[EMAIL_POSITION]."   \t";
     }
 
-    function display_validation($validation){
+    /**
+     * Displays validation massages stored in an array.
+     *
+     * @param array $validation Array with validation data.
+     */
+    function displayValidation($validation){
         $format = '%-15s: %-30s';
         for($i=0; $i<count($validation); $i++){
             echo sprintf($format, $validation[$i]["type"], $validation[$i]["message"]);
         }
     }
 
-    function correct_row($data){
+    /**
+     * Corrects the data in given row (removes whitespaces, capitalize names).
+     *
+     * @param array $data Row (user) data.
+     * @return array $data Corrected user data.
+     */
+    function correctRow($data){
         $data[NAME_POSITION] = ucfirst(strtolower(preg_replace('/\s+/', '', $data[NAME_POSITION])));
         $data[SURNAME_POSITION] = ucfirst(strtolower(preg_replace('/\s+/', '', $data[SURNAME_POSITION])));
         $data[EMAIL_POSITION] = strtolower(preg_replace('/\s+/', '', $data[EMAIL_POSITION]));
         return $data;
     }
 
+    /**
+     * Validates if the value in a row have correct format.
+     *
+     * @param array $data Row (user) data.
+     * @param array $emails emails which has been already processed.
+     * @return array $validation_result array with errors or warnings caused by data.
+     */
     function validate_row($data, &$emails){
         $validation_result = array();
 
@@ -204,8 +274,11 @@
         return $validation_result;
     }
 
-
-    function print_help(){
+    /**
+     * Prints help information for the script.
+     *
+     */
+    function printHelp(){
         echo "Available directives: \n";
         echo "--file <file>\t – the name of the CSV to be parsed \n";
         echo "--create_table\t – builds MySQL users table \n";
@@ -219,25 +292,28 @@
 
     if(array_key_exists("help", $options))
     {
-        print_help();
+        printHelp();
     }
     elseif(array_key_exists("h", $options) && array_key_exists("p", $options) && array_key_exists("u", $options))
     {
-        $pdo = connect_to_DB($options["h"], $options["u"], $options["p"]);
+        $pdo = connectToDb($options["h"], $options["u"], $options["p"]);
         if($pdo) {
             if(array_key_exists("create_table", $options))
             {
-                create_table($pdo);
+                createTable($pdo);
             }
             else
             {
                 if(array_key_exists("file", $options)) 
                 {
-                    parse_file($options["file"], array_key_exists("dry_run", $options), $pdo);
+                    if(file_exists($options["file"]))
+                        parseFile($options["file"], array_key_exists("dry_run", $options), $pdo);
+                    else
+                        echo "File specified does not exist";
                 } 
                 else 
                 {
-                    echo "No file specified";
+                    echo "File not specified";
                 }
             }
         }
@@ -246,6 +322,4 @@
     {
         print("You didn't use the script properly. To transfer data to DB remember to specift: user, database, host and source file.\n  Please type --help to see how to use the script");
     }
-
-
 ?>
